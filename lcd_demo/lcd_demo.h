07d392868523e975cb86cd691df129b4af343fef
nejc.ramovs <nejc.ramovs@9aabe9b7-a576-1165-9547-0d67252868cc>
2010-05-31 14:28:20 +0000

    Progress :)

--- lcd_demo/lcd_demo.h
+++ lcd_demo/lcd_demo.h
@@ -8,5 +8,26 @@
 #ifndef LCD_DEMO_H_
 #define LCD_DEMO_H_
 
+void restore_terminal_settings(void);
+void disable_waiting_for_enter(void);
+char get_key();
+
+void build_vars();
+
+void clear_screen();
+void intro_animation();
+
+void switch_tab(int previous, int current);
+int menu_selection(int position);
+
+void demo_pixel();
+void demo_line();
+void demo_rect();
+void demo_string();
+void demo_bmpfull();
+void demo_bmpsmall();
+void demo_integration();
+void demo_pong();
+
 
 #endif /* LCD_DEMO_H_ */
